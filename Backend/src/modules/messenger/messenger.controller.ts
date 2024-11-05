27e07dc2935cc7c4c465505c4c6b2d1b6b840a45
huan0568 <huan0568@gmail.com>
2024-11-05 16:24:11 +0700

    feat: create new messenger in chatbox endpoint

--- Backend/src/modules/messenger/messenger.controller.ts
+++ Backend/src/modules/messenger/messenger.controller.ts
@@ -1,11 +1,12 @@
 import httpResponseDto from "@/common/dtos/httpResponse.dto";
 import { MessengerService } from "./messenger.service";
 import { HttpResponseBodySuccessDto } from "@/common";
+import { Request, Response } from "express";
 
 export class MessengerController {
   constructor(private readonly messengerService = new MessengerService()) {}
 
-  async findMessengerById(req: any, res: any) {
+  async findMessengerById(req: Request, res: Response) {
     const userId = req.body.userId;
     const messengerId = req.params.id;
 
@@ -15,6 +16,25 @@ export class MessengerController {
     );
     if (result instanceof Error) {
       httpResponseDto.exception(result, res);
+      return;
+    }
+    httpResponseDto.success<any>(
+      result as HttpResponseBodySuccessDto<any>,
+      res
+    );
+  }
+
+  async createMessenger(req: Request, res: Response) {
+    const userId = req.body.userId;
+    const { boxChatId, content } = req.body;
+    const result = await this.messengerService.createMessenger(
+      userId,
+      boxChatId,
+      content
+    );
+    if (result instanceof Error) {
+      httpResponseDto.exception(result, res);
+      return;
     }
     httpResponseDto.success<any>(
       result as HttpResponseBodySuccessDto<any>,
